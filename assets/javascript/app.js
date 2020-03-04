3a2352e084093c01b2f72e2e35a3e454c2953f73
Rachael Gladwin <rachaelgladwin@Rachaels-iMac.local>
2020-03-04 07:37:35 -0700

    everything is working, now just need to get timer fixed, it continues the interval after the next question pops up so it runs all of the timers simultaneously until all timers end lol

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,72 +1,36 @@
-//timer 
-var number = 60;
-var intervalId;
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-}
-//  The decrement function.
-function decrement() {
-    number--;
-    $("#show-number").html("<h2> You have " + number + " seconds left! </h2>");
-    if (number === 0) {
-        stop();
-        alert("Times Up!");
-    }
-}
-//  The stop function
-function stop() {
-    clearInterval(intervalId);
-}
-run();
-
-//game code 
-//create variable for the content of the questions
-var qContent = [
-    {
-        "question": "Judd owns Avenue5.",
-        "answer": "True",
-        "fullAnswer": "He is the Owner. Billionaire. Genius."
-    },
-    {
-        "question": "Clark is not the real captain, Joe is!",
-        "answer": "True",
-        "fullAnswer": "Joe, may he rest in peace... orbiting the ship."
-    },
-    {
-        "question": "Captain Clark is THE real captain.",
-        "answer": "False",
-        "fullAnswer": "Phhhhh.. captain of the ss boozey maybe!"
-    },
-    {
-        "question": "Clark loves Billie!",
-        "answer": "True",
-        "fullAnswer": "Oh you know he does."
-    },
-    {
-        "question": "Clark saved the ship when it was leaking feces",
-        "answer": "False",
-        "fullAnswer": "BILLIE saved the ship okay it was not a joint effort!!"
-    },
-]
-
+//declaring variables to hold the score and values for the game
 var counter = 0,
-    score = 0;
+  score = 0;
 
-// assigning elements
+// variable for quiz content
 
-var $name = $(".name"),
-    $generate = $(".generate"),
-    $result = $(".results"),
-    $score = $(".score"),
-    $thanks = $(".thanks"),
-    $options = $(".options");
+var content = [
+  { "name": "Clark is the real captain.", "answer": "false" },
+  { "name": "Clark in NOT the real captain Joe is.. may he rest in peace.", "answer": "true" },
+  { "name": "Judd owns Avenue5.", "answer": "true" },
+  { "name": "Clark loves Billie", "answer": "true" },
+  { "name": "A baby made the ship beep.", "answer": "true" },
+  { "name": "The ship was built by NASA", "answer": "false" },
+  { "name": "Karen makes a great passenger liason.", "answer": "false" },]
 
-var aveFiveApp = {};
 
-// starts off by showing the "name" value in the first item in the object
-aveFiveApp.init = function() {
-  var selection = qContent[counter];
+//placing variables into HTML elements for display
+
+var $name = $('.name'),
+  $generate = $('.generate'),
+  $result = $('.results'),
+  $score = $('.score'),
+  $thanks = $('.thanks'),
+  $options = $('.options');
+
+var aveApp = {};
+
+// the initial state of the quiz:
+// starts off by showing the "name" value in the first item in the data object
+// hides the 'next' button, results, score and 'thanks for playing' html as a default
+
+aveApp.init = function () {
+  var selection = content[counter];
   type = selection["answer"];
   $name.html(selection["name"]);
   $generate.hide();
@@ -75,69 +39,81 @@ aveFiveApp.init = function() {
   $thanks.hide();
 }
 
-// the function for generating questions and moving quiz along
+// the function for moving through the quiz
 
-aveFiveApp.generate = function() {
-
-  // if there are still questions remaining, show next one
-  
-  if (counter < qContent.length) {
-    var selection = qContent[counter];
+aveApp.generate = function () {
+  // if there are still questions remaining, show the next one
+  if (counter < content.length) {
+    var selection = content[counter];
     $name.html(selection["name"]);
-    type = selection["answer"];  
+    type = selection["answer"];
 
     $result.hide();
     $score.hide();
     $name.show();
     $options.show();
-
-  // if there are no more questions...
-  
+    // if there are no more questions, thank the user for playing and give the option to tweet the score
   } else {
-    $thanks.show()
+    $thanks.show();
   }
-
   $generate.hide();
 }
-
-// determines whether selection was right
-
-$(".choice").click(function(e) {
-  var chosenAnswer = e.target.id;  
+// the event handler that determines whether the user's selection was right
+$('.choice').click(function (e) {
+  var chosenAnswer = e.target.id;
   $result.show();
   $score.show();
   $name.hide();
   $options.hide();
-
   // setting up "full sentence" values for each type -- add else if statements if you have more than two possibilities
-  
-  if (type == "True") {
-    fullAnswer = "True";
+  if (type == "true") {
+    fullAnswer = "true";
   } else {
-    fullAnswer = "False";
+    fullAnswer = "false";
   }
-   
   // tell the user whether they're right or wrong, and add a point to the score if they're right
-
   if (chosenAnswer == type) {
-    $result.html("<span class='right'>By the power of Judd you did it, I mean Judd did it!</span> It's: " + fullAnswer + ".");
+    $result.html("<span class='right'>Affirmative, my fake captain!</span>");
     score++;
   } else {
-    $result.html("<span class='wrong'>I knew you were a fraud CLARK!</span> That's incorrect, it's: " + fullAnswer + ".");
+    $result.html("<span class='wrong'>INCORRECT!!! Judd will end you for this!</span>");
   }
   counter++;
   $score.html("You're " + score + " for " + counter + ".");
   $generate.show();
-  
+
 });
 
-$(document).ready(function() {
-    aveFiveApp.init();
+$(document).ready(function () {
+  aveApp.init();
+
 });
 
-$generate.on("click", function() {
-    aveFiveApp.generate();
-});
+$generate.on('click', function () {
+  aveApp.generate();
+  //timer 
+  var number = 60;
+  var intervalId;
+  function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
 
+  //  The decrement function.
+  function decrement() {
+    number--;
+    $("#show-number").html("<h2> You have " + number + " seconds left! </h2>");
+    if (number === 0) {
+      stop();
+      clearInterval(intervalId);
+      alert("Times Up!");
+    }
+  }
+  //  The stop function
+  function stop() {
+    clearInterval(intervalId);
+  }
+  run();
+});
 
 
